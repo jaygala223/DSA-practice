866642968003c8916206cee6c684ab9934d90334
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-01-22 14:46:50 +0530

    Time: 77 ms (38.45%), Space: 31.2 MB (71.59%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -4,7 +4,7 @@ public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int left = 0, right = arr.size()-1;
         
-        while(right - left >= k){
+        while(right - left+1 > k){
             if(abs(arr[left]-x) <= abs(arr[right]-x)) right--;
             else left++;
         }
