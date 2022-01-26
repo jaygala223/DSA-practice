cf3bbea3e3054b6f2bc4dd9fa8ae8e5f2c79bc02
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-01-26 22:45:33 +0530

    Time: 37 ms (30.96%), Space: 10.4 MB (85.64%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '(') st.push(i);
+            if(s[i] == ')') {
+                if(st.empty() == true) s[i] = '*';
+                else st.pop();
+            }
+        }
+        
+        while(!st.empty()){
+            s[st.top()] = '*';
+            st.pop();
+        }
+        s.erase(remove(s.begin(), s.end(), '*'), s.end());
+        return s;
+    }
+};
