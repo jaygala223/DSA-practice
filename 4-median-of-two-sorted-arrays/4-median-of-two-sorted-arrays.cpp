4f84c55f3c6c360fa42fd4bec39d2eefe20a840f
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-02-02 20:58:21 +0530

    Time: 50 ms (47.50%), Space: 89.3 MB (60.62%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
+        if(nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1); 
         int m=nums1.size();
         int n=nums2.size();
+        int lo = 0, hi = m;        
         
-        if(m+n == 0) return 0.0;
-        vector<int> ans(m+n,0);
-        
-        int i=m-1,j=n-1,k=m+n-1;
-        while(i>=0 and j>=0){
-            if(nums2[j]>nums1[i]){
-                ans[k--]=nums2[j--];
+        while(lo <= hi){
+            int cut1 = (lo+hi) >> 1; //divide by 2
+            int cut2 = ((m+n+1)>>1) - cut1;
+            
+            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+            
+            int right1 = cut1 == m ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n ? INT_MAX : nums2[cut2];
+            
+            if(left1 <= right2 && left2<=right1){
+                if((m+n)%2 == 0) return (max(left1,left2) + min(right1,right2)) / 2.0;
+                else return max(left1, left2);
             }
-            else{
-                ans[k--]=nums1[i--];
+            else if(left1 > right2){
+                hi = cut1 - 1;
             }
+            else lo = cut1+1;
         }
-        while(i>=0){
-            ans[k--]=nums1[i--];
-        }
-        while(j>=0){
-            ans[k--]=nums2[j--];
-        }
-        int mid=(m+n)/2;
-        if((m+n)%2==0){
-            return (ans[mid]+ans[mid-1])/2.0;
-        }
-        else return ans[mid];
+        return 10.0;
     }
 };
