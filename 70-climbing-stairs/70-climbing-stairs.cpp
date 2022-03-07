f4b50375f657b1dd3e0fc237d5e7255f4b3588df
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-03-07 14:45:26 +0530

    Time: 4 ms (20.05%), Space: 5.9 MB (54.67%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dp = vector<int>(46,-1);
+    
+    int climbStairs(int n) {
+        if(n <= 1) return 1;
+        if(dp[n] != -1) return dp[n];
+        
+        //left and right here mean the RECURSIVE TREE'S left and right
+        
+        int left = climbStairs(n-1);
+        int right = climbStairs(n-2);
+        
+        dp[n] = left+right;
+        return left+right;
+    }
+};
