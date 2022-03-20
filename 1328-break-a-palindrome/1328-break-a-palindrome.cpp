362c55b5579dbcd24a5b460f1adee0e34d3df7d5
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-03-20 14:38:12 +0530

    Time: 4 ms (22.24%), Space: 6.1 MB (95.20%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     string breakPalindrome(string palindrome) {
-        //string ans = palindrome;
-        //reverse(ans.begin(),ans.end());
         
-        //not possible to break that palindrome
         if(palindrome.size() == 1) return "";
+        
         int n = palindrome.size();
         
-        for(int i=0; i<palindrome.size()/2; i++){
+        for(int i=0; i<n/2; i++){
             if(palindrome[i] != 'a'){
                 palindrome[i] = 'a';
                 return palindrome;
