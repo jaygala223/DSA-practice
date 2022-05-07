8872356143327c0e8a0967c59640786a9f2a352e
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-05-07 10:43:18 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (52.79%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
+    int binomial(int r, int n){
+        if(r == 0 or r == n) return 1;
+        if(n > n-r) r = n-r;
+        
+        long ans = 1;
+        for(int i=0; i<r; i++){
+            ans*= (n-i);
+            ans/= (i+1);
+        }
+        return int(ans);
+    }
+    
     vector<int> getRow(int rowIndex) {
         vector<int> ans;
         
-        int n = rowIndex;
-        long res = 1; 
-        ans.push_back(int(res));
-        for(int i=0; i<n; i++){
-            res *= (n-i);
-            res /= (i+1);
-            ans.push_back(int(res));
+        for(int i=0; i<=rowIndex; i++){
+            ans.push_back(binomial(i, rowIndex));
         }
+
         return ans;
     }
 };
