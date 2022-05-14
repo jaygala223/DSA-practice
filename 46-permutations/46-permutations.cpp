c3bdd63c3f9152eee603bd46235ffa9d7c9b4c00
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-05-14 08:06:55 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (78.54%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    void func(int n, vector<int> &map, vector<int> &ds, vector<int> &arr, vector<vector<int>> &ans){
-        if(ds.size() == n){
-            ans.push_back(ds);
+    void func(int i, int n, vector<vector<int>> &ans, vector<int> &arr){
+        if(i > n-1){
+            ans.push_back(arr);
             return;
         }
         
-        for(int j=0; j<n; j++){
-            if(map[j] == 0){
-                ds.push_back(arr[j]);
-                map[j] = 1;
-                func(n, map, ds, arr, ans);
-                ds.pop_back();
-                map[j] = 0;    
-            }
+        for(int j=i; j<n; j++){
+            swap(arr[i],arr[j]);
+            func(i+1, n, ans, arr);
+            swap(arr[j],arr[i]);
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
+        
         vector<vector<int>> ans;
-        vector<int> ds;
-        vector<int> map(nums.size(),0);
-        func(nums.size(), map, ds, nums, ans);
+        func(0, nums.size(), ans, nums);
+        
         return ans;
     }
 };
