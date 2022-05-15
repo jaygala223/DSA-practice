eb2f13c56c2e6837861f673ace845a49eccbe7ab
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-05-15 15:55:12 +0530

    Time: 4 ms (89.36%), Space: 7.2 MB (69.21%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,49 +1,27 @@
 class Solution {
 public:
-    bool isSafe(int row, int col, vector<string> &board, int n){
-        int dupcol = col;
-        int duprow = row;
-        
-        //bottom left diagonal safety
-        while(row<n and col>=0){
-            if(board[row][col] == 'Q') return false;
-            row++;
-            col--;
-        }
-        
-        row = duprow;
-        col = dupcol;
-        
-        //left row check
-        while(col >= 0){
-            if(board[row][col] == 'Q') return false;
-            col--;
-        }
-        
-        row = duprow;
-        col = dupcol;
-        
-        //top left diagonal check
-        while(row >= 0 and col >= 0){
-            if(board[row][col] == 'Q') return false;
-            row--;
-            col--;
-        }
-    
-        return true;
-    }
-    
-    void solve(int col, int n, vector<string> &board, vector<vector<string>> &ans){
+    void solve(int col, int n, vector<string> &board, vector<vector<string>> &ans, vector<int> &bottomDiag, vector<int> &upDiag, vector<int> &leftRow){
         if(col > n-1){
             ans.push_back(board);
             return;
         }
         
         for(int row=0; row<n; row++){
-            if(isSafe(row, col, board, n)){
+            if(upDiag[n-1 + col - row] == 0 and bottomDiag[row + col] == 0 and leftRow[row] == 0){
                 board[row][col] = 'Q';
-                solve(col+1, n, board, ans);
+                
+                //set positions to 1 for hashing
+                upDiag[n-1 + col - row] = 1; 
+                bottomDiag[row + col] = 1; 
+                leftRow[row] = 1;
+                
+                solve(col+1, n, board, ans, bottomDiag, upDiag, leftRow);
                 board[row][col] = '.';
+                
+                //backtrack bhi to karlo
+                upDiag[n-1 + col - row] = 0; 
+                bottomDiag[row + col] = 0; 
+                leftRow[row] = 0;
             }
         }
     }
@@ -55,11 +33,14 @@ public:
         vector<string> board(n);
         string s(n, '.');
         
-        for(int i=0; i<n; i++){
-            board[i] = s;
-        }
+        //checking-with-hashing approach
+        vector<int> bottomDiag(2*n - 1,0);
+        vector<int> upDiag(2*n - 1,0);
+        vector<int> leftRow(n,0);
         
-        solve(0, n, board, ans);
+        for(int i=0; i<n; i++) board[i] = s;
+        
+        solve(0, n, board, ans, bottomDiag, upDiag, leftRow);
         return ans;
     }
 };
