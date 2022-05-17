a920bce92a0b2e2dd9aaf4ac90d42b2b32afd4eb
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-05-17 19:22:28 +0530

    Time: 3 ms (44.83%), Space: 7.3 MB (55.86%) - LeetHub

--- 1144-decrease-elements-to-make-array-zigzag/1144-decrease-elements-to-make-array-zigzag.cpp
+++ 1144-decrease-elements-to-make-array-zigzag/1144-decrease-elements-to-make-array-zigzag.cpp
@@ -1,61 +1,20 @@
 class Solution {
 public:
     int movesToMakeZigzag(vector<int>& nums) {
-        int rule1 = 0;
-        int rule2 = 0;
-        if(nums.size()==1) {
-            return 0;
+         int even = 0, odd = 0, n = nums.size();
+    //for odd
+        for(int i=1; i < n; i+= 2){
+            int minVal = min(nums[i-1], i+1 < n ? nums[i+1] : 1001);
+            if(minVal <= nums[i])
+                odd += (nums[i]-minVal+1);
         }
-        vector<int>nums2 = nums;
-        for(int i = 0 ; i < nums.size() ; i=i+2) {
-            if(i==0) {
-                if(nums[0]<=nums[1]) {
-                    int diff = nums[1] - nums[0] + 1;
-                    rule1 += diff;
-                    nums[1] -= diff;
-                }
-            }
-            else if(i==nums.size()-1) {
-                if(nums[i]<=nums[i-1]) {
-                    int diff = nums[i-1] - nums[i] + 1;
-                    rule1 += diff;
-                    nums[i-1] -= diff;
-                }
-            }
-            else{
-                if(nums[i]<=nums[i-1]) {
-                    int diff = nums[i-1] - nums[i] + 1;
-                    rule1 += diff;
-                    nums[i-1] -= diff;
-                }
-                if(nums[i]<=nums[i+1]) {
-                    int diff =nums[i+1] - nums[i] + 1;
-                    rule1 += diff;
-                    nums[i+1] -= diff;
-                }
-            }
+    // For Even
+        for(int i=0; i < n; i+= 2){
+            int minVal = min(i > 0 ? nums[i-1] : 1001, i+1 < n ? nums[i+1] : 1001);
+            
+            if(minVal <= nums[i]) even += (nums[i]-minVal+1);
+               
         }
-        for(int i = 1 ; i < nums2.size() ; i=i+2) {
-            if(i==nums.size()-1) {
-                if(nums2[i]<=nums2[i-1]) {
-                    int diff = nums2[i-1] - nums2[i] + 1;
-                    rule2 += diff;
-                    nums2[i-1] -= diff;
-                }
-            }
-            else{
-                if(nums2[i]<=nums2[i-1]) {
-                    int diff = nums2[i-1] - nums2[i] + 1;
-                    rule2 += diff;
-                    nums2[i-1] -= diff;
-                }
-                if(nums2[i]<=nums2[i+1]) {
-                    int diff =nums2[i+1] - nums2[i] + 1;
-                    rule2 += diff;
-                    nums2[i+1] -= diff;
-                }
-            }
-        }
-        return min(rule1,rule2);
+        return min(even,odd);
     }
 };
