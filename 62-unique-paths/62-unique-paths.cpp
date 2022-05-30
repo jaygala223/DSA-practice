0813f815e9d2c516285f041925a1e4b76541812f
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-05-30 16:29:37 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (57.43%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    int func(int row, int column, int m, int n, vector<vector<int>> &dp){
-        if(row == 0 and column == 0){
-            return 1;
-        }
-        else if(row < 0 or column < 0) return 0;
-        else if(dp[row][column] != -1) return dp[row][column];
-        
-        //since we're going top down dp
-        int up = func(row-1, column, m, n, dp);
-        int left = func(row, column-1, m, n, dp);
-        
-        return dp[row][column] = up + left;
-         
-    }
-    
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
         
-        //tabulization, bottom to up DP
+        vector<int> prev(n, 0);
+        //space optimization
+        
         for(int i=0; i<m; i++){
+            vector<int> cur(n, 0);
+            
             for(int j=0; j<n; j++){
                 if(i == 0 and j == 0) {
-                    dp[i][j] = 1;
+                    cur[j] = 1;
                     continue;
                 }
+                
                 int up = 0;
-                if(i-1>=0) up = dp[i-1][j];
+                if(i > 0) up = prev[j];
                 int left = 0;
-                if(j-1>=0) left = dp[i][j-1];
-                dp[i][j] = left + up;
+                
+                if(j > 0) left = cur[j-1];
+                cur[j] = left + up;
             }
+            prev = cur;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
