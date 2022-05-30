a73b8e3a6eed8bc0670cc2393e63a163e1de128f
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-05-30 15:45:41 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (44.55%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    //int ans = 0;
-    int bfs(int row, int column, int m, int n, vector<vector<int>> &dp){
+    int func(int row, int column, int m, int n, vector<vector<int>> &dp){
         if(row == 0 and column == 0){
             return 1;
         }
         else if(row < 0 or column < 0) return 0;
         else if(dp[row][column] != -1) return dp[row][column];
         
-        //since we're going bottom up dp
-        int up = bfs(row-1, column, m, n, dp);
-        int left = bfs(row, column-1, m, n, dp);
+        //since we're going top down dp
+        int up = func(row-1, column, m, n, dp);
+        int left = func(row, column-1, m, n, dp);
         
         return dp[row][column] = up + left;
          
@@ -18,6 +17,21 @@ public:
     
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return bfs(m-1,n-1,m,n, dp);
+        
+        //tabulization, bottom to up DP
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 and j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int up = 0;
+                if(i-1>=0) up = dp[i-1][j];
+                int left = 0;
+                if(j-1>=0) left = dp[i][j-1];
+                dp[i][j] = left + up;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
