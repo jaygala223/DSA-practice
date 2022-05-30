2133909f391778755a65fd046b1a2ac71a667251
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-05-30 14:26:57 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.28%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    //int ans = 0;
+    int bfs(int row, int column, int m, int n, vector<vector<int>> &dp){
+        if(row == 0 and column == 0){
+            return 1;
+        }
+        if(row < 0 or column < 0) return 0;
+        if(dp[row][column] != -1) return dp[row][column];
+        
+        //since we're going bottom up dp
+        int up = bfs(row-1, column, m, n, dp);
+        int left = bfs(row, column-1, m, n, dp);
+        
+        return dp[row][column] = up + left;
+         
+    }
+    
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return bfs(m-1,n-1,m,n, dp);
+    }
+};
