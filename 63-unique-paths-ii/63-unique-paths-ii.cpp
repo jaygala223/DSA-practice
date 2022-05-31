15cf39ab772e63a80a43615a863ce38b0e7c81df
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-05-31 13:36:57 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (12.66%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -16,11 +16,27 @@ public:
         return dp[i][j] = left + up;
     }
     
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
         
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        return func(m-1, n-1, obstacleGrid, dp);
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 and j == 0) dp[i][i] = 1;
+                else{
+                    int up = 0, left = 0;
+                    
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
+                    
+                    dp[i][j] = left + up;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+        //return func(m-1, n-1, obstacleGrid, dp);
     }
 };
