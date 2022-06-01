655b02eed348b2b0ba03d535c33b4601c39821a7
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-06-01 15:38:32 +0530

    Time: 9 ms (78.60%), Space: 10.2 MB (6.54%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -19,8 +19,23 @@ public:
         
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        
-        return func(m-1, n-1, grid[m-1][n-1], grid, dp);
+        vector<vector<int>> dp(m, vector<int>(n,0));
+    
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 and j == 0) {
+                    dp[i][j] = grid[0][0];
+                    continue;
+                }
+                
+                int left = 201, up = 201;
+                
+                if(j>0) left = grid[i][j] + dp[i][j-1];
+                if(i>0) up =  grid[i][j] + dp[i-1][j];
+                
+                dp[i][j] = min(left, up);
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
