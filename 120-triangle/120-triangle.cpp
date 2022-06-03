4a91422b560ae511a6ffb1671a345202ed5eb6fd
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-06-03 09:39:12 +0530

    Time: 8 ms (60.18%), Space: 9 MB (26.17%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -15,8 +15,19 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
         
-        vector<vector<int>> dp(m, vector<int>(m,-1));
+        vector<vector<int>> dp(m, vector<int>(m,0));
         
-        return func(0, 0, m, triangle, dp);
+        for(int j=0;j<m; j++) dp[m-1][j] = triangle[m-1][j];
+        
+        for(int i=m-2; i>=0; i--){
+            for(int j=0; j<=i; j++){
+                int down = triangle[i][j] + dp[i+1][j];
+                int diag = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down, diag);
+            }
+        }
+        
+        return dp[0][0];
+        //return func(0, 0, m, triangle, dp);
     }
 };
