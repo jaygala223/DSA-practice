b1ab9926b88eee6b4e29694e76912dfcad7ffa07
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-06-15 19:17:01 +0530

    Time: 161 ms (41.87%), Space: 13.1 MB (83.98%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool checkPossible(string &s1, string &s2){
+        if(s1.size() != s2.size() + 1) return false;
+        
+        int p1 = 0, p2 = 0;
+        
+        while(p1 < s1.size()){
+            if(s1[p1] == s2[p2]) p2++;
+            
+            p1++;
+        }
+        if(p1 == s1.size() and p2 == s2.size()) return true;
+        return false;
+    }
+    
+    
+    static bool comp(string &s1, string &s2){
+        return s1.size() < s2.size();
+    }
+    
+    int longestStrChain(vector<string>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end(), comp);
+        vector<int> dp(n, 1);
+        int maxi = 1;
+        
+        for(int i=0; i<n; i++){
+            for(int prev=0; prev<i; prev++){
+                
+                if(checkPossible(arr[i], arr[prev])){
+                    dp[i] = max(1+dp[prev], dp[i]);
+                }
+            }
+            maxi = max(maxi, dp[i]);
+        }
+        return maxi;
+    }
+};
