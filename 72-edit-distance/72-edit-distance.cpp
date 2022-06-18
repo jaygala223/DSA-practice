3005d0b46daebd0f6bf84afe4dcf327a5f6e8cc6
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-06-18 10:00:51 +0530

    Time: 28 ms (34.75%), Space: 9 MB (37.80%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -18,8 +18,32 @@ public:
         int n1 = word1.size();
         int n2 = word2.size();
         
-        vector<vector<int>> dp(n1, vector<int> (n2, -1));
+        vector<vector<int>> dp(n1+1, vector<int> (n2+1, 0));
         
-        return f(n1-1, n2-1, word1, word2, dp);
+        
+        //if(ind1 < 0) return ind2+1;
+        //if(ind2 < 0) return ind1+1;
+        for(int ind1=0; ind1<=n1; ind1++) dp[ind1][0] = ind1;
+        for(int ind2=0; ind2<=n2; ind2++) dp[0][ind2] = ind2;
+        
+        for(int ind1=1; ind1<=n1; ind1++){
+            for(int ind2=1; ind2<=n2; ind2++){
+                if(word1[ind1-1] == word2[ind2-1]) dp[ind1][ind2] = dp[ind1-1][ind2-1];
+        
+                //not match
+                else{
+                    int op1 = 1e8, op2 = 1e8, op3 = 1e8;
+                    op1 = dp[ind1-1][ind2];
+                    op2 = dp[ind1][ind2-1];
+                    op3 = dp[ind1-1][ind2-1];
+
+                    dp[ind1][ind2] = 1 + min(op1, min(op2, op3));    
+                }
+                
+            }
+        }
+        
+        return dp[n1][n2];
+        //return f(n1-1, n2-1, word1, word2, dp);
     }
 };
