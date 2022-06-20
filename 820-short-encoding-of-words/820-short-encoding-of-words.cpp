a428945d4cc51c516b3bedd203cdfc98c30a50db
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-06-20 18:58:48 +0530

    Time: 70 ms (73.27%), Space: 17.4 MB (81.19%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -3,16 +3,15 @@ public:
     int minimumLengthEncoding(vector<string>& words) {
         if(words.size() == 1) return words[0].size()+1;
         
-        unordered_set<string> set;
-        for(auto item: words){set.insert(item);}
+        unordered_set<string> set(words.begin(), words.end());
         
-        for(int i=0; i<words.size(); i++){
-            for(int ind=1; ind<words[i].size(); ind++){
-                string s = words[i].substr(ind, words[i].size()-1);
+        for(auto word: set){
+            for(int ind=1; ind<word.size(); ind++){
+                string s = word.substr(ind, word.size()-1);
                 if(set.find(s) != set.end()) set.erase(s);
             }
         }
-        int n = 0, cnt = 0;
+        int n = 0;
         for(auto item: set) n+= item.size();
         
         return n+set.size();
