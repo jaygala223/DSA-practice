2891a8569d55e93bac0509a161e67ca2952da00b
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-06-25 16:28:02 +0530

    Time: 42 ms (46.41%), Space: 27.1 MB (53.59%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int n = nums.size();
+        int mini = nums[0];
+        bool changed = false;
+        
+        if(n == 1 or n == 2) return true;
+        
+        for(int i=0; i<n-1; i++){
+            if(nums[i] > nums[i+1]){
+                if(changed) return false;
+                if(i == 0 or nums[i+1] >= nums[i-1]) nums[i] = nums[i+1];
+                else nums[i+1] = nums[i];
+                changed = true;
+            }
+        }
+        
+        return true;
+    }
+};
