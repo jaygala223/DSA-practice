eab3348f5700258da94970fc76e5f1c8edb1f4d7
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-06-27 16:49:40 +0530

    Time: 27 ms (95.03%), Space: 13.6 MB (65.18%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -4,6 +4,7 @@ public:
         int maxi = INT_MIN;
         for(auto ch: n){
             maxi = max(maxi, ch - '0');
+            if(maxi == 9) return maxi;
         }
         
         return maxi;
