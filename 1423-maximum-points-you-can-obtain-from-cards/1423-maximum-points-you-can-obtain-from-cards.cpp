ab588bd8209b8aa7c6e75f9dc222119913bd8863
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-06-27 10:14:16 +0530

    Time: 78 ms (67.50%), Space: 44.5 MB (11.76%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,11 +1,28 @@
 class Solution {
 public:
-    int maxScore(vector<int>& C, int K) {
-        int total = 0;
-        for (int i = 0; i < K; i++) total += C[i];
-        int best = total;
-        for (int i = K - 1, j = C.size() - 1; ~i; i--, j--)
-            total += C[j] - C[i], best = max(best, total);
-        return best;
+    int maxScore(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        int totSum = accumulate(nums.begin(), nums.end(), 0);
+        
+        if(k == n) return totSum;
+        
+        int len = n - k, mini = INT_MAX, subSum = 0;
+        queue<int> q;
+        
+        for(int i=0; i<n; i++){
+            subSum += nums[i];
+            q.push(nums[i]);
+            if(q.size() == len) mini = min(subSum, mini);
+            
+            else if(q.size() > len){
+                subSum -= q.front();
+                q.pop();
+                mini = min(subSum, mini);
+            }
+            
+        }
+        cout<<mini<<" ";
+        return totSum - mini;
     }
 };
