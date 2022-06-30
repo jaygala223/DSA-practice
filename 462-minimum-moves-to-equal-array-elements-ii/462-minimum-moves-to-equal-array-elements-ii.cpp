7ca88eccb2c30bba06c729af292fc7978ec442f1
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-06-30 11:27:39 +0530

    Time: 7 ms (97.98%), Space: 10.9 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -3,14 +3,13 @@ public:
     int minMoves2(vector<int>& nums) {
         int n = nums.size();
         
+        int median = 0, ans = 0;
+        if(n%2 == 0) median += (n/2)-1;
+        else median += ((n+1)/2)-1;
+        
         sort(nums.begin(), nums.end());
         
-        int median = 0, ans = 0;
-        if(n%2 == 0) median = (n/2) - 1;
-        else median = ((n+1)/2) - 1;
-        cout<<median;
         for(int i=0; i<n; i++){
-            if(i == median) continue;
             ans += abs(nums[median] - nums[i]);
         }
         return ans;
