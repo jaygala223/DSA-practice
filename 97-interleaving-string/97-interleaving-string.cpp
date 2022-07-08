f242a7b3865aea1668c0849083973193a95cb2ee
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-07-08 23:49:37 +0530

    Time: 9 ms (47.16%), Space: 7.2 MB (42.87%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool f(int i, int j, int k, string &s1, string &s2, string &s3, vector<vector<int>> &dp){
-        if(i == 0) return 1;
+        if(i == s3.size()) return 1;
         if(dp[j][k] != -1) return dp[j][k];
         
         int first = 0; 
         int second = 0;
-        if(j-1 >= 0 and s3[i-1] == s1[j-1]) first = f(i-1, j-1, k, s1, s2, s3, dp);
-        if(k-1 >= 0 and s3[i-1] == s2[k-1]) second = f(i-1, j, k-1, s1, s2, s3, dp);
+        if(j < s1.size() and s3[i] == s1[j]) first = f(i+1, j+1, k, s1, s2, s3, dp);
+        if(k < s2.size() and s3[i] == s2[k]) second = f(i+1, j, k+1, s1, s2, s3, dp);
         
         return dp[j][k] = (first || second);
     }
@@ -20,6 +20,6 @@ public:
         vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
         
         if(len != n + m) return false;
-        return f(len, n, m, s1, s2, s3, dp);
+        return f(0, 0, 0, s1, s2, s3, dp);
     }
 };
