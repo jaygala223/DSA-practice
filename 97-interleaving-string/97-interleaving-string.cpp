62cb73598b73bf1b84cb365665be406eb034432c
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-07-09 12:29:13 +0530

    Time: 13 ms (37.18%), Space: 7.1 MB (45.51%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -17,12 +17,30 @@ public:
         int m = s2.size();
         int len = s3.size();
         
-        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
-        
         if(len != n + m) return false;
         
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        
+        //dp[n][m] = 1;
+        
+        for(int j=0; j<=n; j++){
+            for(int k=0; k<=m; k++){
+                
+                if(k == 0 and j == 0) dp[j][k] = 1;
+                
+                else{
+                    int first = 0, second = 0;
+                    if(j>0 and s3[j+k-1] == s1[j-1]) first = dp[j-1][k];
+                    if(k>0 and s3[j+k-1] == s2[k-1]) second = dp[j][k-1];
+
+                    dp[j][k] = (first || second);
+
+                }
+                
+            }
+        }
         
         
-        return f(0, 0, 0, s1, s2, s3, dp);
+        return dp[n][m]; 
     }
 };
