a4a6da5d9d7993a20508c83fd00592b9dd0df389
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-07-21 12:51:58 +0530

    Time: 496 ms (5.00%), Space: 73 MB (44.95%) - LeetHub

---
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int lcs(string text1, string text2) {
+        int n1 = text1.size();
+        int n2 = text2.size();
+        
+        //string s = "";
+        
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        
+        for(int j=0; j<=n2; j++) dp[0][j] = 0;
+        for(int i=0; i<=n1; i++) dp[i][0] = 0;
+        
+        for(int ind1=1; ind1<=n1; ind1++){
+            for(int ind2=1; ind2<=n2; ind2++){
+                
+                //match
+                if(text1[ind1-1] == text2[ind2-1]) {
+                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                    //s = s + text1[ind1-1];
+                } 
+                    
+                //not match then
+                else{
+                    dp[ind1][ind2] = max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
+                    //if(dp[ind1-1][ind2] > dp[ind1][ind2-1]) s = s + text1[ind1-1];
+                    //else s = s + text2[ind2-1];
+                } 
+            }
+        }
+        //string ans = s.substr(0, dp[n1][n2]);
+        //if(dp[n1][n2]) cout<<ans<<endl;
+        return dp[n1][n2];
+    }
+    
+    int longestPalindromeSubseq(string s) {
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        return lcs(s, s2);
+    }
+};
