97f4fd48f5b1c16866ded991d9c9c511c683e0bd
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-09-07 17:53:23 +0530

    Time: 63 ms (66.97%), Space: 14 MB (82.01%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -9,17 +9,9 @@ class Solution:
         while(lo < hi):
             mid = int(lo + (hi-lo)/2)
             
-            if(mid == n-1):
-                return mid if nums[mid] >= nums[mid-1] else mid-1
-                
-            elif(mid == 0):
-                return mid if nums[mid] >= nums[mid+1] else mid+1
-            
-            elif(nums[mid] >= nums[mid+1] and nums[mid] >= nums[mid-1]):
-                return mid
-            
-            if(nums[mid] < nums[mid-1]): hi = mid-1;
-            else: lo = mid+1
+            if(nums[mid] < nums[mid+1]):
+                lo = mid+1
+            else: hi = mid
             
         return lo
         
