6e81cc075e073266aede51e65cec7d7e8c4c49ca
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-09-10 11:35:40 +0530

    Time: 1564 ms (39.58%), Space: 13.9 MB (90.22%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -4,21 +4,26 @@ class Solution:
         ansLen = 0
         
         for i in range(len(s)):
-            l,r = i,i
-            #odd len
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r-l+1 > ansLen:
-                    ansLen = r-l+1
+            
+            #odd
+            l, r = i, i
+            while l>=0 and r<len(s) and s[l] == s[r]:
+                if r-l + 1 > ansLen:
+                    ansLen = r-l + 1
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
+                
+            
+            
+            #even len
+            l, r = i, i+1
+            while l>=0 and r<len(s) and s[l] == s[r]:
+                if r-l + 1 > ansLen:
+                    ansLen = r-l + 1
                     res = s[l:r+1]
                 l -= 1
                 r += 1
             
-            #even len
-            l,r = i, i+1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if r-l+1 > ansLen:
-                    ansLen = r-l+1
-                    res = s[l:r+1]
-                l -= 1
-                r += 1
         return res
+            
