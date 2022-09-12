da24aca28677363027ef2f390db1a58cbfd2cba4
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-09-12 17:58:36 +0530

    Time: 78 ms (32.72%), Space: 14.1 MB (43.10%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -1,20 +1,48 @@
 class Solution:
+    
+    #top down memoized
+#         def helper(row, col, grid, dp):
+#             if row < 0 or col < 0 or grid[row][col] == 1: return 0
+            
+#             if row == 0 and col == 0: return 1
+#             if (row,col) in dp: return dp[(row, col)]
+            
+#             left = helper(row, col-1, grid, dp)
+#             up = helper(row-1, col, grid, dp)
+            
+#             dp[(row, col)] = left+up
+#             return dp[(row, col)]
+    
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
         
-        dp = {}
+        #dp = {}       
         
-        def helper(row, col, grid, dp):
-            if row < 0 or col < 0 or grid[row][col] == 1: return 0
-            
-            if row == 0 and col == 0: return 1
-            if (row,col) in dp: return dp[(row, col)]
-            
-            left = helper(row, col-1, grid, dp)
-            up = helper(row-1, col, grid, dp)
-            
-            dp[(row, col)] = left+up
-            return dp[(row, col)]
+        #bottom up - tabulated
+        #dp[(0,0)] = 1
         
-        return helper(m-1, n-1, obstacleGrid, dp)
+        if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1] == 1: return 0
+        
+        dp = [[0]* n for _ in range(m)]
+        dp[0][0] = 1
+        
+        
+        for i in range(1,m):
+            dp[i][0] = 1 if obstacleGrid[i][0]==0 and dp[i-1][0]==1 else 0
+            
+        for j in range(1,n):
+            dp[0][j] = 1 if obstacleGrid[0][j]==0  and dp[0][j-1]==1 else 0
+        
+        for row in range(1, m):
+            for col in range(1, n):
+                if obstacleGrid[row][col] != 1: 
+
+                    left = dp[row][col-1]
+                    up = dp[row-1][col]
+
+                    dp[row][col] = left+up
+        
+        
+        
+        return dp[m-1][n-1]
