2232acb2b57b3e3e869ef435d99f896a4802a392
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-09-14 09:50:56 +0530

    Time: 58 ms (32.48%), Space: 13.8 MB (70.88%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -3,14 +3,13 @@ class Solution:
         n = len(nums)
         if n == 1: return nums[0]
         def robhouses(nums):
-            n = len(nums)
-            dp = [0]*n
-            dp[0] = nums[0]
-            for i in range(1, n):
-                pick = nums[i]
-                if i>1: pick += dp[i-2]
-                not_pick = 0 + dp[i-1]
-                dp[i] = max(pick, not_pick)
-            return dp[n-1]
+            rob1, rob2 = 0,0
+            
+            for item in nums:
+                temp = max(item + rob1, rob2)
+                rob1 = rob2
+                rob2 = temp
+                
+            return rob2
         
         return max(robhouses(nums[1:]), robhouses(nums[:n-1]))
