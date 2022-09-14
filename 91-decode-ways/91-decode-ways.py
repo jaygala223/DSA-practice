ee48cd2c28c8313d4b72452f107ac9cdba8426cf
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-09-14 11:44:01 +0530

    Time: 61 ms (28.66%), Space: 14.2 MB (10.50%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -13,10 +13,18 @@ class Solution:
             res = func(i+1)
             if i+1 < len(s) and (s[i]=="1" or 
                (s[i]=="2" and s[i+1] in "0123456")):
-                
                 res += func(i+2)
             
             dp[i] = res
             return dp[i]
             
-        return func(0)
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == '0':
+                dp[i] = 0
+            else:
+                dp[i] = dp[i+1]
+                if i+1 < len(s) and (s[i]=="1" or 
+               (s[i]=="2" and s[i+1] in "0123456")):
+                    dp[i] += dp[i+2]
+            
+        return dp[0]
