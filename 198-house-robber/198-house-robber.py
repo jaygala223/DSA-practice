1b99cf4c287cd04cfb000d1b3627d8546d2a55bf
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-09-14 09:39:25 +0530

    Time: 68 ms (10.25%), Space: 13.9 MB (65.32%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -11,7 +11,15 @@ class Solution:
             dp[i] = max(pick, not_pick)
             return dp[i]
 
-        dp = [-1]*len(nums)
+        dp = [0]*len(nums)
         
-        return helper(len(nums)-1, nums, dp)
+        dp[0] = nums[0]
+        
+        for i in range(1, len(nums)):
+            pick = nums[i]
+            if i>1: pick += dp[i-2]
+            not_pick = 0 + dp[i-1]
+            dp[i] = max(pick, not_pick)
+        
+        return dp[len(nums)-1]
             
