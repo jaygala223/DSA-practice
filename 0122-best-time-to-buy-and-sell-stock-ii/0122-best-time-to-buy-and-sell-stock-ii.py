b6d3b938137f4bd362724d5276e8e659490bcf59
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-12-23 14:28:40 +0530

    Time: 226 ms (5.05%), Space: 44.7 MB (5.24%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        self.dp = [[-1]*2]*len(prices)
+        
+        def f(ind, buy):
+            #base
+            if ind == len(prices): return 0
+            if (ind, buy) in cache: return cache[(ind,buy)]
+            
+            profit = 0
+            if buy:
+                profit = max(f(ind+1, 0) - prices[ind], f(ind+1, 1) - 0)
+            else:
+                profit = max(f(ind+1, 1) + prices[ind], f(ind+1, 0) - 0)
+                
+            cache[(ind, buy)] = profit
+            return profit
+            
+        cache = dict()
+        
+        return f(0, 1)
