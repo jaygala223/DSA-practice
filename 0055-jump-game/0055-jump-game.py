44df8ea358d67298e2841ca6ae5c65cd10662527
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-12-26 17:31:57 +0530

    Time: 423 ms (80.42%), Space: 14.4 MB (69.98%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -4,15 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        
-        n = len(nums)
-        
         reach, i = 0, 0
         
-        while(i < n):
+        while(i < len(nums)):
             if i > reach:
                 break
             reach = max(reach, i + nums[i])
             i+=1
             
-        return i == n
+        return i == len(nums)
