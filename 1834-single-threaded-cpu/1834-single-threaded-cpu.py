36412ba248e838ffbc88fc92006e44c3e2d393d6
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-12-29 18:08:34 +0530

    Time: 1836 ms (94.44%), Space: 64.3 MB (16.67%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -5,13 +5,21 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        heap, result, time = [], [], 0
-        for i, enqueueTime, processTime in sorted([(i, et, pt) for i, (et, pt) in enumerate(tasks)], key=lambda x: (x[1], x[2], x[0])):
-            while heap and time < enqueueTime:
-                nextTaskProcessTime, nextTaskIndex, nextTaskEnqueueTime = heapq.heappop(heap)
-                result.append(nextTaskIndex)
-                time = max(time, nextTaskEnqueueTime) + nextTaskProcessTime
-            heapq.heappush(heap, (processTime, i, enqueueTime))
-        while heap:
-            result.append(heapq.heappop(heap)[1])
-        return result
+        tasks = sorted([(t[0], t[1], i) for i, t in enumerate(tasks)])
+        ans = []
+        i = 0
+        heap = []
+        time = tasks[0][0]
+        
+        while len(ans) < len(tasks):
+            while i < len(tasks) and tasks[i][0] <= time:
+                heapq.heappush(heap, (tasks[i][1], tasks[i][2]))
+                i += 1
+            if heap: #available tasks hai
+                t, idx = heapq.heappop(heap)
+                time += t
+                ans.append(idx)
+            elif i < len(tasks):
+                time = tasks[i][0]
+            
+        return ans
