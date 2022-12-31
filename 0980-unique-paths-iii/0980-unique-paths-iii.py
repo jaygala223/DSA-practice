436d8a41e28190f61878404a8f28416f1df135c4
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-12-31 14:22:27 +0530

    Time: 47 ms (98.00%), Space: 13.9 MB (93.68%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -10,12 +10,10 @@ class Solution:
                     start_i, start_j = (i, j)
                 if grid[i][j] == 0 or grid[i][j] == 1:
                     zeros += 1
-        
 
         def dfs(i, j, zeros):
-            if i < 0 or j < 0 or i == len(grid) or j == len(grid[0]):
+            if i < 0 or j < 0 or i == len(grid) or j == len(grid[0]) or grid[i][j] < 0:
                 return 0
-            if grid[i][j] < 0: return 0
             if grid[i][j] == 2:
                 self.ans += zeros == 0
                 return
