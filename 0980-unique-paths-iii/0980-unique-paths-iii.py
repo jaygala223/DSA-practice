f9abb65c4f3c60454f55bc9b5f71ac7ef14f64d0
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2022-12-31 14:20:31 +0530

    Time: 56 ms (91.68%), Space: 14 MB (54.63%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -0,0 +1,32 @@
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        self.ans = 0
+        
+        start_i, start_j, zeros = 0, 0, 1
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    start_i, start_j = (i, j)
+                if grid[i][j] == 0:
+                    zeros += 1
+        
+
+        def dfs(i, j, zeros):
+            if i < 0 or j < 0 or i == len(grid) or j == len(grid[0]):
+                return 0
+            if grid[i][j] < 0: return 0
+            if grid[i][j] == 2:
+                self.ans += zeros == 0
+                return
+            
+            dirs = [(-1,0), (0,1), (0,-1), (1,0)]
+            
+            grid[i][j] = -2
+            for row,col in dirs:
+                dfs(i + row, j + col, zeros-1)
+            grid[i][j] = 0
+            
+        dfs(start_i, start_j, zeros)
+        return self.ans
+            
