a88546ab5ea7a5a3a89adfc6cdb8bde8932fb212
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2023-01-02 09:59:15 +0530

    Time: 35 ms (81.79%), Space: 13.9 MB (56.30%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,4 +1,11 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         
-        return word.lower() == word or word.upper() == word or word == ''.join([word[0].upper(), word[1:].lower()])
+        # return word.lower() == word or word.upper() == word or word == ''.join([word[0].upper(), word[1:].lower()])
+        
+        if len(word) < 2: return True
+        if ord(word[0]) < 97:
+            word = word[1:]
+            return ord(max(word)) < 97 or ord(min(word)) >= 97
+        else:
+            return ord(min(word)) >= 97
