213d06658ee1b8162c4a18eee198109eadeb1963
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2023-01-04 12:17:04 +0530

    Time: 2885 ms (10.17%), Space: 29.2 MB (14.59%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumRounds(self, tasks: List[int]) -> int:
+        
+        freq, ans = {}, 0
+        
+        for item in tasks:
+            freq[item] = freq.get(item, 0) + 1
+        print(freq)
+        
+        for item in freq:
+            if freq[item] != 1:
+                ans += (freq[item] + 2 )//3
+            else:
+                return -1
+            
+        return ans
