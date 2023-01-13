0af561d83cf8bee12d8744c3ca1ff160d7858f35
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2023-01-13 10:21:57 +0530

    Time: 2485 ms (63.97%), Space: 157.9 MB (31.38%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -6,10 +6,12 @@ class Solution:
         for i in range(1, len(parent)):
             tree[parent[i]].append(i)
         
+        #to store ans globally and not pass it in the function
         res = [-1]
         
         def dfs(node):
             candi = [0]
+            
             for nei in tree[node]:
                 cur = dfs(nei)
                 if s[nei] != s[node]:
