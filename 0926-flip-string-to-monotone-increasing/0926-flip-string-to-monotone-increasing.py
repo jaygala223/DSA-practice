f781176a9c58048d935c49ecaad58fa2156eb7b9
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2023-01-17 21:02:02 +0530

    Time: 143 ms (91.35%), Space: 14.9 MB (93.27%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -1,6 +1,7 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        
+        #https://www.youtube.com/watch?v=dw79Uf4s3cA
+        #https://leetcode.com/problems/flip-string-to-monotone-increasing/discuss/3061225/Python-3-oror-7-lines-w-explanation-and-example-oror-TM%3A-100-96
         ans, ones = 0, 0
         
         for c in s:
