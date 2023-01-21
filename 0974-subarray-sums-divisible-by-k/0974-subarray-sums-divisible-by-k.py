d47f6d1975e5e7538ed7ee1563b987fdade6aeda
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2023-01-21 18:53:02 +0530

    Time: 304 ms (65.83%), Space: 18.9 MB (17.42%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,6 +1,7 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        
+        ###https://www.youtube.com/watch?v=7Xeorb721LQ&t=735s
+        ###https://leetcode.com/problems/subarray-sums-divisible-by-k/discuss/3070481/Python3-prefix-sum-%2B-frequency-table-(Explained)
         ans = 0
         
         remainderFreq = defaultdict(int)
