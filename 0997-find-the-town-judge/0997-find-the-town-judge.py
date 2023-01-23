f9527ec3de7581fec8f25cddb9e30bff228e1618
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2023-01-23 10:46:54 +0530

    Time: 1178 ms (35.13%), Space: 18.8 MB (90.33%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -12,3 +12,6 @@ class Solution:
             if indegree[i] == n-1 and outdegree[i] == 0:
                 return i
         return -1
+    
+    #TC: O(n) + O(n) ~= O(n) --> to form graph and looping over 1 to n
+    #SC: O(2n) => Indegree and outdegree
