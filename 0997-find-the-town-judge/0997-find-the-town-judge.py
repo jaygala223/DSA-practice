4889001b867cd49eba3f1f5612648c5ec9d8cf03
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2023-01-23 10:50:00 +0530

    Time: 730 ms (92.11%), Space: 19 MB (59.07%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,17 +1,16 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         
-        indegree = defaultdict(int)
-        outdegree = defaultdict(int)
+        count = defaultdict(int)
         
         for p, c in trust:
-            indegree[c] += 1
-            outdegree[p] += 1
+            count[c] += 1
+            count[p] -= 1
         
         for i in range(1, n+1):
-            if indegree[i] == n-1 and outdegree[i] == 0:
+            if count[i] == n-1:
                 return i
         return -1
     
     #TC: O(n) + O(n) ~= O(n) --> to form graph and looping over 1 to n
-    #SC: O(2n) => Indegree and outdegree
+    #SC: O(n) => using count
