72ce3635f31dbc7d014b4e041f5505b1ee9324d5
Jay Gala <57001778+jaygala223@users.noreply.github.com>
2023-01-31 23:17:26 +0530

    Time: 9443 ms (5.68%), Space: 213.4 MB (5.67%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,6 +1,8 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
         #can be thought of as a LIS problem
+        #recursive tc: 2^n ... similar to subsequences problem
+        #memo: n^2
         
         n = len(ages)
         ans = -1e9
@@ -20,14 +22,17 @@ class Solution:
             curr_age = players[ind][1]
             curr_score = players[ind][0]
             
+            take, not_take = 0, 0
+            
             if curr_age >= maxAge:
                 take = curr_score + func(ind + 1, curr_age)
-                not_take = 0 + func(ind + 1, maxAge)
-                dp[(ind, maxAge)] = max(take, not_take)
-                return max(take, not_take)
-            
-            dp[(ind, maxAge)] = func(ind + 1, maxAge)
-            return func(ind + 1, maxAge)
+                # not_take = 0 + func(ind + 1, maxAge)
+                # dp[(ind, maxAge)] = max(take, not_take)
+                # return max(take, not_take)
+                
+            not_take = 0 + func(ind + 1, maxAge)
+            dp[(ind, maxAge)] = max(take, not_take)
+            return max(take, not_take)
         
         return func(0, 0)
         
